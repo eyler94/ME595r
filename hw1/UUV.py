50f86b5514ea84a1df93fb9ee033cff9ce1ef2a3
Michael Eyler <eyler94@gmail.com>
2019-09-18 12:43:29 -0600

    Maybe everthing is done?

--- hw1/UUV.py
+++ hw1/UUV.py
@@ -56,8 +56,8 @@ class UUV():
         self.z = 0. + np.random.randn() * np.sqrt(self.Q)
 
     def propagate_dynamics(self, u):
-        self.X = self.A @ self.X_1 + self.B * u #+ np.array([[np.random.randn() * 2. * np.sqrt(self.sigma_pos)], [np.random.randn() * 2. * np.sqrt(self.sigma_vel)]])
-        self.mu = self.A @ self.X_1 + self.B * u + np.array([[np.random.randn() * 2. * np.sqrt(self.sigma_pos)], [np.random.randn() * 2. * np.sqrt(self.sigma_vel)]])
+        self.X = self.A @ self.X_1 + self.B * u + np.array([[np.random.randn() * 2. * np.sqrt(self.sigma_pos)], [np.random.randn() * 2. * np.sqrt(self.sigma_vel)]])
+        self.mu = self.X # self.A @ self.X_1 + self.B * u + np.array([[np.random.randn() * 2. * np.sqrt(self.sigma_pos)], [np.random.randn() * 2. * np.sqrt(self.sigma_vel)]])
         self.X_1 = self.X
         return self.mu
 
