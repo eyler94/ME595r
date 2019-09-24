4cee461f0a7da2dcd87253dbc4e4db88f6d06a79
Michael Eyler <eyler94@gmail.com>
2019-09-24 11:58:21 -0600

    Plotter works. Still working on Dynamics and plotting end figures.

--- hw2/Plotter.py
+++ hw2/Plotter.py
@@ -7,22 +7,67 @@ pi = np.pi
 
 class Plotter:
     def __init__(self, x=-5, y=-3, theta=pi/2, width=20, height=20, lm=np.array([[0], [0]])):
-        self.x = x
-        self.y = y
-        self.theta = theta
+        # Properties of the world
         self.lm = lm
         self.width = width
         self.height = height
-        circle = np.arange(0., 2 * np.pi, 0.01)
-        x = np.cos(circle)
-        x = np.hstack([0., x])
-        y = np.sin(circle)
-        y = np.hstack([0., y])
-        self.points = np.array([x, y])
-        self.fig = plt.figure(1)
-        self.hmt = np.array([[np.cos(self.theta), -np.sin(self.theta), self.x], [np.sin(self.theta), np.cos(self.theta), self.y], [0., 0., 1.]])
 
+        # Properties of the robot
+        self.x_loc = x
+        self.y_loc = y
+        self.theta = theta
+
+        # Points to draw the robot
+        circle = np.arange(0., 2 * pi, 0.01)
+        self.x_points = np.cos(circle)
+        self.x_points = np.hstack([0., self.x_points])
+        self.y_points = np.sin(circle)
+        self.y_points = np.hstack([0., self.y_points])
+        self.points = np.array([self.x_points, self.y_points])
+        self.hmt = np.array(
+            [[np.cos(self.theta), -np.sin(self.theta), self.x_loc],
+             [np.sin(self.theta), np.cos(self.theta), self.y_loc],
+             [0., 0., 1.]])
+        self.points = self.hmt @ np.array([self.x_points, self.y_points, np.ones([1, self.x_points.size])])
+
+        # First plot
+        plt.ion()
+        fig2 = plt.figure(2)
+        plt.plot(self.lm[0], self.lm[1], 'x', color='black')
+        plt.plot(self.points[0].T, self.points[1].T)
+        plt.axis([-self.width / 2., self.width / 2., -self.height / 2., self.height / 2.])
+        plt.draw()
+        plt.pause(0.001)
+        plt.clf()
 
     def update(self, x, y, theta):
-        print("Update")
+        self.x_loc = x
+        self.y_loc = y
+        self.theta = theta
+        self.calc_points()
 
+        # Re-plot
+        fig2 = plt.figure(2)
+        plt.plot(self.lm[0], self.lm[1], 'x', color='black')
+        plt.plot(self.points[0].T, self.points[1].T)
+        plt.axis([-self.width / 2., self.width / 2., -self.height / 2., self.height / 2.])
+        plt.draw()
+        plt.pause(0.001)
+        plt.clf()
+
+    def calc_points(self):
+        self.hmt_2d()
+        self.points = self.hmt @ np.array([self.x_points, self.y_points, np.ones([1, self.x_points.size])])
+
+    def hmt_2d(self):
+        self.hmt = np.array(
+            [[np.cos(self.theta), -np.sin(self.theta), self.x_loc],
+             [np.sin(self.theta), np.cos(self.theta), self.y_loc],
+             [0., 0., 1.]])
+
+# # Example of how to plot with
+# for iter in np.arange(0., 50., 0.75):
+#     Xr = np.cos(2*np.pi*iter/50.)
+#     Yr = np.sin(2*np.pi*iter/50.)
+#     Theta = np.pi/2.+2*np.pi*iter/50.
+#     plot.update(Xr, Yr, Theta)
--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,15 +1,16 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import sys
 sys.path.append('..')
 import R2D2
 import World
-import Plotter as Plotter
+import Plotter
 
 
 # Instantiate World, Robot, and EKF
 R2D2 = R2D2.R2D2()
 World = World.World()
-plot = Plotter.Plotter(R2D2.x0, R2D2.y0, R2D2.theta0, World.width, World.height, World.Landmarks)
+Plotter = Plotter.Plotter(R2D2.x0, R2D2.y0, R2D2.theta0, World.width, World.height, World.Landmarks)
 
 
 # Set Timeline
@@ -18,31 +19,21 @@ Ts = 0.1    # Sec
 time_data = np.arange(0., Tf, Ts)
 
 # Generate Truth
+[X, Y, TH] = R2D2.propagateDynamics(time_data)
+
+# fig = plt.figure(1)
+# plt.plot(X, t)
+# plt.show
+# print("Hey.")
+
 
 # Generate Filter
 
 # Plot
-import matplotlib.pyplot as plt
-import time
-circle = np.arange(0., 2*np.pi, 0.01)
-x = np.cos(circle)
-x = np.hstack([0., x])
-y = np.sin(circle)
-y = np.hstack([0., y])
+for iter in range(0, X.size):
+    Plotter.update(X[iter], Y[iter], TH[iter])
 
-plt.ion()
 
-for iter in np.arange(0., 50., 0.75):
-    Xr = np.cos(2*np.pi*iter/50.)
-    Yr = np.sin(2*np.pi*iter/50.)
-    Theta = np.pi/2.+2*np.pi*iter/50.
-    hmt = np.array([[np.cos(Theta), -np.sin(Theta), Xr], [np.sin(Theta), np.cos(Theta), Yr], [0., 0., 1.]])
-    points = hmt @ np.array([x, y, np.ones([1, x.size])])
-    plt.plot(points[0].T, points[1].T)
-    plt.axis([-World.width/2., World.width/2., -World.height/2., World.height/2.])
-    plt.draw()
-    plt.pause(0.00001)
-    plt.clf()
 
-input("Press the any key... goodluck.")
-
+#
+# input("Press the any key... goodluck.")
