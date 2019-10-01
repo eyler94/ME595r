b74712859e8c9eb46a259d5f2b58314f6c2f6e4b
Michael Eyler <eyler94@gmail.com>
2019-10-01 09:17:43 -0600

    Began copying directories and building structure for hw3.

---
+++ UKF/EKF.py
@@ -0,0 +1,91 @@
+import numpy as np
+import math
+
+
+def wrapper(ang):
+    if ang > np.pi:
+        # print("Too much.")
+        ang = ang - 2 * np.pi
+    elif ang <= -np.pi:
+        # print("Too little.")
+        ang = ang + 2 * np.pi
+    return ang
+
+
+class EKF:
+    def __init__(self, R2D2, World):
+        print("Init")
+        v = 1
+        omega = 1
+        theta = 1
+        self.ts = R2D2.ts
+        th_om_dt = wrapper(theta + omega * self.ts)
+        self.alpha1 = R2D2.alpha1
+        self.alpha2 = R2D2.alpha2
+        self.alpha3 = R2D2.alpha3
+        self.alpha4 = R2D2.alpha4
+        self.G = np.array([[1, 0, - v / omega * np.cos(theta) + v / omega * np.cos(th_om_dt)],
+                           [0, 1, - v / omega * np.sin(theta) + v / omega * np.sin(th_om_dt)],
+                           [0, 0, 1]])
+        self.V = np.array([[(-np.sin(theta)+np.sin(th_om_dt))/omega, v*(np.sin(theta)-np.sin(th_om_dt))/omega**2 + v*np.cos(th_om_dt)*self.ts/omega],
+                           [(np.cos(theta)-np.cos(th_om_dt))/omega, -v*(np.cos(theta)-np.cos(th_om_dt))/omega**2 + v*np.sin(th_om_dt)*self.ts/omega],
+                           [0., self.ts]])
+        self.M = np.array([[self.alpha1 * v ** 2 + self.alpha2 * omega ** 2, 0],
+                           [0, self.alpha3 * v ** 2 + self.alpha4 * omega ** 2]])
+        self.mu = np.array([[R2D2.x0],
+                            [R2D2.y0],
+                            [R2D2.theta0]])
+        self.mu_bar = self.mu + np.array([[- v / omega * np.sin(theta) + v / omega * np.sin(th_om_dt)],
+                                          [v / omega * np.cos(theta) - v / omega * np.cos(th_om_dt)],
+                                          [omega*self.ts]])
+        self.SIG = np.diag([1, 1, 0.1])
+        self.SIG_bar = self.G @ self.SIG @ self.G.T + self.V @ self.M @ self.V.T
+        self.Q = np.array([[R2D2.sigma_r**2, 0],
+                           [0, R2D2.sigma_theta**2]])
+        self.landmarks = World.Landmarks
+        self.num_landmarks = World.Number_Landmarks
+
+
+    def update(self, mu, SIG, v, omega, r, ph):
+        self.mu = mu
+        self.SIG = SIG
+        theta = self.mu[2][0]
+        th_om_dt = wrapper(theta + omega * self.ts)
+        self.G = np.array([[1, 0, - v / omega * np.cos(theta) + v / omega * np.cos(th_om_dt)],
+                           [0, 1, - v / omega * np.sin(theta) + v / omega * np.sin(th_om_dt)],
+                           [0, 0, 1]])
+        self.V = np.array([[(-np.sin(theta)+np.sin(th_om_dt))/omega, v*(np.sin(theta)-np.sin(th_om_dt))/omega**2 + v*np.cos(th_om_dt)*self.ts/omega],
+                           [(np.cos(theta)-np.cos(th_om_dt))/omega, -v*(np.cos(theta)-np.cos(th_om_dt))/omega**2 + v*np.sin(th_om_dt)*self.ts/omega],
+                           [0., self.ts]])
+        self.M = np.array([[self.alpha1 * v ** 2 + self.alpha2 * omega ** 2, 0],
+                           [0, self.alpha3 * v ** 2 + self.alpha4 * omega ** 2]])
+        self.mu_bar = self.mu + np.array([[- v / omega * np.sin(theta) + v / omega * np.sin(th_om_dt)],
+                                          [v / omega * np.cos(theta) - v / omega * np.cos(th_om_dt)],
+                                          [omega*self.ts]])
+        self.SIG_bar = self.G @ self.SIG @ self.G.T + self.V @ self.M @ self.V.T
+        self.features(r, ph)
+
+        self.mu = self.mu_bar
+        self.SIG = self.SIG_bar
+
+        return self.mu, self.SIG
+
+    def features(self, r, ph):
+        for spot in range(0, self.num_landmarks):
+            diff_x = self.landmarks[0][spot]-self.mu_bar[0][0]
+            diff_y = self.landmarks[1][spot]-self.mu_bar[1][0]
+            q = diff_x ** 2 + diff_y ** 2
+            z_hat = np.array([[np.sqrt(q)],
+                              [wrapper(math.atan2(diff_y, diff_x)-self.mu_bar[2][0])]])
+            H = np.array([[-diff_x/np.sqrt(q), -diff_y/np.sqrt(q), 0],
+                          [diff_y/q, -diff_x/q, -1]])
+            S = H @ self.SIG_bar @ H.T + self.Q
+            K = self.SIG_bar @ H.T @ np.linalg.inv(S)
+            z_diff = np.array([r[spot]-z_hat[0],
+                               wrapper(ph[spot]-z_hat[1])])
+            self.mu_bar = self.mu_bar + K @ z_diff
+            self.SIG_bar = (np.eye(3) - K @ H) @ self.SIG_bar
+
+
+
+
---
+++ hw3/hw2.py
@@ -0,0 +1,117 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+sys.path.append('..')
+import R2D2
+import World
+import Plotter
+from EKF import EKF
+from importlib import reload
+reload(R2D2)
+reload(World)
+reload(Plotter)
+reload(EKF)
+
+# Instantiate World, Robot, Plotter, and EKF
+R2D2 = R2D2.R2D2()
+World = World.World()
+Plotter = Plotter.Plotter(R2D2.x0, R2D2.y0, R2D2.theta0, World.width, World.height, World.Landmarks)
+EKF = EKF.EKF(R2D2, World)
+
+# Set Timeline
+Tf = 20     # Sec
+Ts = 0.1    # Sec
+time_data = np.arange(0., Tf, Ts)
+time_data = time_data.reshape([1, 200])
+
+# Generate Truth and Calculate "Noisy Measurements"
+X = np.zeros([1, time_data.size])
+Y = np.zeros([1, time_data.size])
+TH = np.zeros([1, time_data.size])
+
+R = np.zeros([World.Number_Landmarks, time_data.size]) # Range Measurements
+PH = np.zeros([World.Number_Landmarks, time_data.size]) # Bearing Measurements
+
+X[0][0] = R2D2.x0
+Y[0][0] = R2D2.y0
+TH[0][0] = R2D2.theta0
+rph = R2D2.calculate_measurements(World.Number_Landmarks, World.Landmarks)
+R[:, 0] = rph[0].reshape([World.Number_Landmarks, ])
+PH[:, 0] = rph[1].reshape([World.Number_Landmarks, ])
+
+for iter in range(1, 200):
+    xyth = R2D2.propagate_dynamics(time_data[0][iter])
+    X[0][iter] = xyth[0]
+    Y[0][iter] = xyth[1]
+    TH[0][iter] = xyth[2]
+
+    rph = R2D2.calculate_measurements(World.Number_Landmarks, World.Landmarks)
+    R[:, iter] = rph[0].reshape([World.Number_Landmarks, ])
+    PH[:, iter] = rph[1].reshape([World.Number_Landmarks, ])
+
+
+# Filter Data
+MU_X = np.zeros([1, time_data.size])
+MU_Y = np.zeros([1, time_data.size])
+MU_TH = np.zeros([1, time_data.size])
+SIG_X = np.zeros([1, time_data.size])
+SIG_Y = np.zeros([1, time_data.size])
+SIG_TH = np.zeros([1, time_data.size])
+
+R2D2.update_velocity(time_data[0][0])
+mu_sig = EKF.update(EKF.mu, EKF.SIG, R2D2.v_c, R2D2.omega_c, R[:, 0], PH[:, 0])
+MU_X[0][0] = mu_sig[0][0]
+MU_Y[0][0] = mu_sig[0][1]
+MU_TH[0][0] = mu_sig[0][2]
+SIG_X[0][0] = 2 * np.sqrt(mu_sig[1][0][0])
+SIG_Y[0][0] = 2 * np.sqrt(mu_sig[1][1][1])
+SIG_TH[0][0] = 2 * np.sqrt(mu_sig[1][2][2])
+
+
+
+for iter in range(1, 200):
+    R2D2.update_velocity(time_data[0][iter])
+    mu_sig = EKF.update(EKF.mu, EKF.SIG, R2D2.v_c, R2D2.omega_c, R[:, iter], PH[:, iter])
+    MU_X[0][iter] = mu_sig[0][0]
+    MU_Y[0][iter] = mu_sig[0][1]
+    MU_TH[0][iter] = mu_sig[0][2]
+    SIG_X[0][iter] = 2 * np.sqrt(mu_sig[1][0][0])
+    SIG_Y[0][iter] = 2 * np.sqrt(mu_sig[1][1][1])
+    SIG_TH[0][iter] = 2 * np.sqrt(mu_sig[1][2][2])
+
+# Plot
+plt.ion()
+plt.interactive(False)
+
+for iter in range(0, X.size):
+    Plotter.update(X[0][iter], Y[0][iter], TH[0][iter])
+
+
+fig3 = plt.figure(3)
+fig3.clf()
+plt.plot(X.T, Y.T)
+plt.plot(MU_X.T, MU_Y.T)
+plt.title('Path')
+
+fig4 = plt.figure(4)
+fig4.clf()
+plt.plot(time_data[0], MU_X[0]-X[0])
+plt.plot(time_data[0], SIG_X[0])
+plt.plot(time_data[0], -SIG_X[0])
+plt.title('Error in X')
+
+fig5 = plt.figure(5)
+fig5.clf()
+plt.plot(time_data[0], MU_Y[0]-Y[0])
+plt.plot(time_data[0], SIG_Y[0])
+plt.plot(time_data[0], -SIG_Y[0])
+plt.title('Error in Y')
+
+fig6 = plt.figure(6)
+fig6.clf()
+plt.plot(time_data[0], MU_TH[0]-TH[0])
+plt.plot(time_data[0], SIG_TH[0])
+plt.plot(time_data[0], -SIG_TH[0])
+plt.title('Error in Theta')
+plt.show()
+
