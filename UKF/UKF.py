b78ff948fd103f5f9cbca232322f6e49c1055df4
Michael Eyler <eyler94@gmail.com>
2019-10-03 22:02:40 -0600

    Finished init function, seems to run fine. Now to write the update function.

--- UKF/UKF.py
+++ UKF/UKF.py
@@ -10,23 +10,29 @@ def wrapper(ang):
 
 class UKF:
     def __init__(self, R2D2, World):
+        self.v = 1
+        self.omega = 1
+        self.theta = 1
+        self.u = np.array([[self.v],
+                      [self.omega]])
+        self.z = np.array([[0],
+                           [0]])
+
         # Generate augmented mean and covariance
-        v = 1
-        omega = 1
-        theta = 1
         self.ts = R2D2.ts
         self.alpha1 = R2D2.alpha1
         self.alpha2 = R2D2.alpha2
         self.alpha3 = R2D2.alpha3
         self.alpha4 = R2D2.alpha4
-        self.M = np.array([[self.alpha1 * v ** 2 + self.alpha2 * omega ** 2, 0],
-                           [0, self.alpha3 * v ** 2 + self.alpha4 * omega ** 2]])
+        self.M = np.array([[self.alpha1 * self.v ** 2 + self.alpha2 * self.omega ** 2, 0],
+                           [0, self.alpha3 * self.v ** 2 + self.alpha4 * self.omega ** 2]])
         self.Q = np.array([[R2D2.sigma_r**2, 0],
                            [0, R2D2.sigma_theta**2]])
         self.mu = np.array([[R2D2.x0],
                             [R2D2.y0],
                             [R2D2.theta0]])
-        self.mu_a = np.array([R2D2.x0, R2D2.y0, R2D2.theta0, 0, 0, 0, 0])
+        self.mu_a = np.hstack([self.mu.T, np.zeros([1, 4])])
+        self.mu_a = np.reshape(self.mu_a, [7, 1])
         self.mu_a = np.reshape(self.mu_a,[7,1])
         self.SIG = np.diag([1, 1, 0.1])
         self.SIG_a = block_diag(self.SIG, self.M, self.Q)
@@ -45,11 +51,7 @@ class UKF:
         self.Chi_z = self.Chi_a[5:, :]
 
         # Pass sigma points through motion model and compute Gaussian statistics
-        self.u = np.array([[0],
-                           [0]])
-        u = np.array([[v],
-                      [omega]])
-        self.Chi_bar = self.g(u+self.Chi_u,self.Chi_x)
+        self.Chi_bar = self.g(self.u+self.Chi_u,self.Chi_x)
 
         # Calculate weights
         self.w_m = np.ones([1,15])
@@ -64,18 +66,19 @@ class UKF:
         self.SIG_bar = np.multiply(self.w_c,(self.Chi_bar-self.mu_bar)) @ (self.Chi_bar-self.mu_bar).T
 
         # Predict observations at sigma points and compute Gaussian statistics
-        self.Z_bar = self.h(self.Chi_bar, world.Landmarks[:,0]) + self.Chi_z
+        self.Z_bar = self.h(self.Chi_bar, World.Landmarks[:,0]) + self.Chi_z
         self.z_hat = self.Z_bar @ self.w_m.T
         self.S = np.multiply(self.w_c,(self.Z_bar-self.z_hat)) @ (self.Z_bar-self.z_hat).T
         self.SIG_xz = np.multiply(self.w_c,(self.Chi_bar-self.mu_bar)) @ (self.Z_bar-self.z_hat).T
 
         # Update mean and covariance
         self.K = self.SIG_xz @ np.linalg.inv(self.S)
-        # # z_diff = np.array([r[spot] - z_hat[0],
-        # #                    wrapper(ph[spot] - z_hat[1])])
-        # z_diff =
+        z_diff = self.z-self.z_hat
+        z_diff[1] = wrapper(z_diff[1])
         self.mu = self.mu_bar + self.K @ z_diff
         self.SIG = self.SIG_bar - self.K @ self.S @ self.K.T
+        self.lines4_16_wo_7(World.Landmarks[:,1])
+        self.lines4_16_wo_7(World.Landmarks[:,2])
 
     def g(self,u,state):
         print("Propagate sigma points.")
@@ -92,18 +95,40 @@ class UKF:
 
         return x, y, theta
 
-
     def h(self, state, landmark):
         print("Collect sigma measurements.")
         x = landmark[0] - state[0]
         y = landmark[1] - state[1]
 
         r = np.sqrt(x**2 + y**2)
-        ph = wrapper(math.atan2(y,x)- state[2])
+        ph = np.arctan2(y,x)- state[2]
 
         return r, ph
 
-
+    def lines4_16_wo_7(self, landmark):
+        print("Lines 4 through 16 without 7.")
+        # Generate augmented mean and covariance
+        self.mu_a = np.hstack([self.mu.T, np.zeros([1, 4])])
+        self.mu_a = np.reshape(self.mu_a,[7,1])
+        self.SIG_a = block_diag(self.SIG, self.M, self.Q)
+        # Generate Sigma points
+        self.Chi_a = np.hstack([self.mu_a, self.mu_a+self.gamma*ch(self.SIG_a), self.mu_a-self.gamma*ch(self.SIG_a)])
+        self.Chi_x = self.Chi_a[0:3,:]
+        self.Chi_u = self.Chi_a[3:5, :]
+        self.Chi_z = self.Chi_a[5:, :]
+        self.mu_bar = self.Chi_x @ self.w_m.T
+        self.SIG_bar = np.multiply(self.w_c,(self.Chi_bar-self.mu_bar)) @ (self.Chi_bar-self.mu_bar).T
+        # Predict observations at sigma points and compute Gaussian statistics
+        self.Z_bar = self.h(self.Chi_bar, landmark) + self.Chi_z
+        self.z_hat = self.Z_bar @ self.w_m.T
+        self.S = np.multiply(self.w_c,(self.Z_bar-self.z_hat)) @ (self.Z_bar-self.z_hat).T
+        self.SIG_xz = np.multiply(self.w_c,(self.Chi_bar-self.mu_bar)) @ (self.Z_bar-self.z_hat).T
+        # Update mean and covariance
+        self.K = self.SIG_xz @ np.linalg.inv(self.S)
+        z_diff = self.z - self.z_hat
+        z_diff[1] = wrapper(z_diff[1])
+        self.mu = self.mu_bar + self.K @ z_diff
+        self.SIG = self.SIG_bar - self.K @ self.S @ self.K.T
 
     def update(self, mu, SIG, v, omega, r, ph):
         self.mu = mu
