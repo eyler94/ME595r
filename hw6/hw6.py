67525deeb53099578ccd0617f05776b225767a6b
Michael Eyler <eyler94@gmail.com>
2019-11-07 21:55:47 -0700

    Added landmark estimates, now to animate the covariance over time.

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -22,11 +22,11 @@ def wrapper(ang):
     return ang
 
 
-# Instantiate World, Robot, Plotter, and EKF
+# Instantiate World, Robot, Plotter, and ekfslam
 R2D2 = R2D2.R2D2(FoV=np.pi)
-World = World.World(rand=True, num_lm=10)
+World = World.World(rand=True, num_lm=15)
 Plotter = Plotter.Plotter(R2D2.x0, R2D2.y0, R2D2.theta0, World.width, World.height, World.Landmarks)
-EKF = EKF_SLAM.EkfSlam(R2D2, World)
+ekfslam = EKF_SLAM.EkfSlam(R2D2, World)
 
 # Set Timeline
 Tf = 50  # Sec
@@ -56,26 +56,31 @@ for spot in range(0, int(Tf / Ts)):
 MU_X = np.zeros([1, time_data.size])
 MU_Y = np.zeros([1, time_data.size])
 MU_TH = np.zeros([1, time_data.size])
+MU_LM = np.zeros([World.Number_Landmarks*2, time_data.size])
 SIG_X = np.zeros([1, time_data.size])
 SIG_Y = np.zeros([1, time_data.size])
 SIG_TH = np.zeros([1, time_data.size])
+SIG_LM = np.zeros([time_data.size, World.Number_Landmarks*2, World.Number_Landmarks*2])
 
 for spot in range(0, int(Tf / Ts)):
     R2D2.update_velocity(time_data[0][spot])
-    mu_sig = EKF.update(EKF.mu, EKF.SIG, R2D2.v_c, R2D2.omega_c, R[:, spot], PH[:, spot])
-    MU_X[0][spot] = mu_sig[0][0]
-    MU_Y[0][spot] = mu_sig[0][1]
-    MU_TH[0][spot] = mu_sig[0][2]
-    SIG_X[0][spot] = 2 * np.sqrt(mu_sig[1][0][0])
-    SIG_Y[0][spot] = 2 * np.sqrt(mu_sig[1][1][1])
-    SIG_TH[0][spot] = 2 * np.sqrt(mu_sig[1][2][2])
+    ekfslam.update(ekfslam.mu, ekfslam.SIG, R2D2.v_c, R2D2.omega_c, R[:, spot], PH[:, spot])
+    MU_X[0][spot] = ekfslam.mu[0]
+    MU_Y[0][spot] = ekfslam.mu[1]
+    MU_TH[0][spot] = ekfslam.mu[2]
+    MU_LM[:, spot][None] = ekfslam.mu[3::].T
+    SIG_X[0][spot] = 2 * np.sqrt(ekfslam.SIG[0][0])
+    SIG_Y[0][spot] = 2 * np.sqrt(ekfslam.SIG[1][1])
+    SIG_TH[0][spot] = 2 * np.sqrt(ekfslam.SIG[2][2])
 
 # Plot
 plt.ion()
 plt.interactive(False)
 
-# for spot in range(0, X.size):
+for spot in range(0, X.size):
+    Plotter.update_with_path_and_lm(X[0][spot], Y[0][spot], TH[0][spot], X[0][:spot], Y[0][:spot], MU_X[0][:spot], MU_Y[0][:spot], MU_LM[:,spot])
 #     Plotter.update_with_path(X[0][spot], Y[0][spot], TH[0][spot], X[0][:spot], Y[0][:spot], MU_X[0][:spot], MU_Y[0][:spot])
+
 
 fig3 = plt.figure(3)
 fig3.clf()
@@ -103,4 +108,8 @@ plt.plot(time_data[0], wrapper(MU_TH[0] - TH[0]))
 plt.plot(time_data[0], SIG_TH[0])
 plt.plot(time_data[0], -SIG_TH[0])
 plt.title('Error in Theta')
+
+fig7 = plt.figure(7)
+fig7.clf()
+plt.imshow(ekfslam.SIG, "Greys")
 plt.show()
