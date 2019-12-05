89c589a74202d90ade7f474a05d29920da34297e
Michael Eyler <eyler94@gmail.com>
2019-12-04 19:54:04 -0700

    Removed redunancies and unneccessary comments.

--- hw9/hw9.py
+++ hw9/hw9.py
@@ -17,16 +17,12 @@ meas_p2 = np.diag([0.3, 0.7, 0])
 
 predict_mat = linalg.block_diag(np.array([[0.2, 0.8], [0.8, 0.2]]), 0)
 
-num_parts = 10000
+num_parts = 1000
 step_size = 1 / num_parts
 
-prob_orig = np.ones([3, num_parts + 1])
-prob_orig[0] = np.arange(0, 1 + step_size, step_size)
-prob_orig[1] = np.arange(1, 0 - step_size, -step_size)
-#
-# prob_special = deepcopy(prob_orig)
-# prob_special[0] = prob_special[0] * -0.6 + 0.8
-# prob_special[1] = prob_special[0] * 0.6 + 0.2
+prob = np.ones([3, num_parts + 1])
+prob[0] = np.arange(0, 1 + step_size, step_size)
+prob[1] = np.arange(1, 0 - step_size, -step_size)
 
 DEBUG = False
 
@@ -36,11 +32,8 @@ def printer(string):
         print(string)
 
 
-def maxi(eq_given):  # , special=False):
+def pruner(eq_given):  # , special=False):
     printer("maximizing")
-    prob = deepcopy(prob_orig)
-    # if special:  # for special type of max
-    #     prob = deepcopy(prob_special)
     res = eq_given @ prob
     max_val = np.amax(res, axis=0)
     eqs_used = np.unique(np.argmax(res, axis=0))
@@ -51,12 +44,11 @@ def maxi(eq_given):  # , special=False):
 def sensing(eq_given):
     printer("sensing")
     num_max = len(eq_given)
-    # print("nm", num_max)
-    res1 = eq_given @ meas_p1
-    res2 = eq_given @ meas_p2
+    result_1 = eq_given @ meas_p1
+    result_2 = eq_given @ meas_p2
     pad = [(spot, spot) for spot in range(num_max)]
     indices = np.array(list(perms(range(num_max), 2)) + pad).T
-    res_tot = res1[indices[0]] + res2[indices[1]]
+    res_tot = result_1[indices[0]] + result_2[indices[1]]
     return res_tot
 
 
@@ -75,14 +67,17 @@ eq = np.array([[-100, 100, 0],
                [100, -50, 0],
                [0, 0, -1]])
 
-for T in range(20):
-    # # With Pruning
-    eq_max = maxi(eq)
-    eq_sense = maxi(sensing(eq_max))
-    eq = maxi(prediction(eq_sense))
+for T in range(2):
+    # With Pruning
+    eq_max = pruner(eq)
+    eq_sense = pruner(sensing(eq_max))
+    eq = pruner(prediction(eq_sense))
+
     # Without Pruning
     # eq_max = eq
     # eq_sense = sensing(eq_max)
     # eq = prediction(eq_sense)
-    print(T)
-print("eq", eq)
+    # print(T)
+print(eq)
+
+
